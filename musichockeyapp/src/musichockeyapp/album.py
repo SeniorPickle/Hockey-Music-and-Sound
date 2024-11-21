aed0446c9b49bab3f40a47d6a486e62d3971cb02
SeniorPickle <smithr@cbchs.org>
2024-11-21 13:59:01 -0600

    combined

--- musichockeyapp/src/musichockeyapp/album.py
+++ musichockeyapp/src/musichockeyapp/album.py
@@ -21,21 +21,33 @@ class Album():
         self.app.delete_album(self.number)
 
     def open_album(self, what=None):
-        self.albumn_box = toga.Box(style=Pack(direction=COLUMN))
 
-        music_add_box = toga.Box(style=Pack(background_color="#686868"))
+        self.app.current_page = "open_album"
+
+        self.music_add_box = toga.Box(style=Pack(background_color="#800000"))
         music_back_button = toga.Button("X", style=Pack(text_align=LEFT, font_size=10, padding=(5, 0, 5, 20)),on_press=self.app.main_page)
-        music_add_label = toga.Label(self.formal_name, style=Pack(flex=1, background_color="#686868", color="#ffffff",text_align=CENTER, font_size=20))
+        music_add_label = toga.Label(self.formal_name, style=Pack(flex=1, background_color="#800000", color="#ffffff",text_align=CENTER, font_size=15))
         music_add_button = toga.Button("+", style=Pack(text_align=RIGHT, font_size=10, padding=(5, 20, 5, 0)))
-        music_add_box.add(music_back_button)
-        music_add_box.add(music_add_label)
-        music_add_box.add(music_add_button)
+        self.music_add_box.add(music_back_button)
+        self.music_add_box.add(music_add_label)
+        self.music_add_box.add(music_add_button)
 
         music_list_box = toga.Box(style=Pack(direction=COLUMN, flex=1))
-        music_list_scroll_box = toga.ScrollContainer(vertical=True, style=Pack(direction=COLUMN, flex=1),content=music_list_box)
+        self.music_list_scroll_box = toga.ScrollContainer(vertical=True, style=Pack(direction=COLUMN, flex=1),content=music_list_box)
 
-        self.albumn_box.add(music_add_box)
-        self.albumn_box.add(music_list_scroll_box)
-        self.albumn_box.add(self.app.sound_board_box)
+        self.refresh_album_box()
+
+
+    def refresh_album_box(self, what=None):
+        self.albumn_box = toga.Box(style=Pack(direction=COLUMN))
+
+        self.albumn_box.add(self.music_add_box)
+        self.albumn_box.add(self.app.black_line_border_box1)
+        self.albumn_box.add(self.music_list_scroll_box)
+        if self.app.sound_board_open == True:
+            self.albumn_box.add(self.app.black_line_border_box3)
+            self.albumn_box.add(self.app.sound_board_scroll_box)
+        self.albumn_box.add(self.app.black_line_border_box4)
+        self.albumn_box.add(self.app.sound_board_button_box)
 
         self.app.refresh_page(self.albumn_box)
--- musichockeyapp/src/musichockeyapp/app.py
+++ musichockeyapp/src/musichockeyapp/app.py
@@ -71,8 +71,24 @@ class MusicHockeyApp(App):
         We then create a main window (with a name matching the app), and
         show the main window.
         """
+        
+        project_dir = self.paths.data
+        album_dir = os.path.join(project_dir, 'albums')
+        if not os.path.exists(album_dir):
+                os.makedirs(album_dir)
 
         self.albums=[]
+        for entry in os.scandir(album_dir):
+            if entry.is_dir():
+                print(entry.name)
+                self.albums.append(Album(entry.name, self, len(self.albums)))
+        project_dir = self.paths.data
+        album_dir = os.path.join(project_dir, 'albums')
+        if not os.path.exists(album_dir):
+                os.makedirs(album_dir)
+        
+        self.sound_board_open = False
+        self.current_page = None
         self.add_album_questions = False
         self.main_page()
         
@@ -84,8 +100,28 @@ class MusicHockeyApp(App):
             self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = content
         self.main_window.show()
+    
+    def refresh_box(self, what=None):
+        self.box = toga.Box(style=Pack(direction=COLUMN))
+
+        #this is the compile wherer you add all the parts together
+        self.box.add(self.album_add_box)
+        self.box.add(self.black_line_border_box1)
+        if self.add_album_questions == True:
+            self.box.add(self.album_questions_box)
+            self.box.add(self.black_line_border_box2)
+        self.box.add(self.album_list_scroll_box)
+        if self.sound_board_open == True:
+            self.box.add(self.black_line_border_box3)
+            self.box.add(self.sound_board_scroll_box)
+        self.box.add(self.black_line_border_box4)
+        self.box.add(self.sound_board_button_box)
+
+        self.refresh_page(self.box)
 
     def _import(self, widget):
+        self.current_page= "_import"
+
         width, height = self.main_window.size
         import_box_outer = toga.Box(style=Pack(direction=COLUMN))
 
@@ -158,53 +194,62 @@ class MusicHockeyApp(App):
             self.status_label.text = f"An error occurred: {e}"
 
     def main_page(self, what=None):
-        self.box = toga.Box(style=Pack(direction=COLUMN))
 
-        album_add_box = toga.Box(style=Pack(background_color="#686868"))
-        album_fill_label = toga.Label("", style=Pack(flex=1, background_color="#686868"))
+        self.current_page= "main_page"
+
+        self.current_page= "main_page"
+
+        #these are border lines
+        self.black_line_border_box1 = toga.Box(style=Pack(height=4,background_color="#000000"))
+        self.black_line_border_box2 = toga.Box(style=Pack(height=4, background_color="#4e4e4e"))
+        self.black_line_border_box3 = toga.Box(style=Pack(height=4, background_color="#4e4e4e"))
+        self.black_line_border_box4 = toga.Box(style=Pack(height=4, background_color="#000000"))
+
+
+         #this is the top bar that contains the add and import button
+        self.album_add_box = toga.Box(style=Pack(background_color="#800000"))
+        album_fill_label = toga.Label("", style=Pack(flex=1, background_color="#800000"))
         album_import_button = toga.Button("Import", on_press=self._import, style=Pack(text_align=RIGHT, font_size=10, padding=(5, 5, 5, 0)))
         album_add_button = toga.Button("+", style=Pack(text_align=RIGHT, font_size=10, padding=(5, 20, 5, 5)),on_press=self.open_album_questions)
-        album_add_box.add(album_fill_label)
-        album_add_box.add(album_import_button)
-        album_add_box.add(album_add_button)
+        self.album_add_box.add(album_fill_label)
+        self.album_add_box.add(album_import_button)
+        self.album_add_box.add(album_add_button)
 
-        album_questions_box = toga.Box(style=Pack(background_color="#7b7b7b"))
-        album_questions_label = toga.Label("", style=Pack(flex=1.5, background_color="#7b7b7b"))
-        album_questions_answers_box = toga.Box(style=Pack(direction=COLUMN, background_color="#7b7b7b", flex=1))
-        album_questions_input_box = toga.Box(style=Pack(background_color="#7b7b7b", padding=(0,0,0,0)))
-        album_questions_name_input_title = toga.Label("Album name:", style=Pack(text_align=RIGHT, font_size=10, background_color="#7b7b7b", color="#ffffff", padding = (10, 0, 5, 5)))
-        self.album_questions_name_input = toga.TextInput(style=Pack(flex=1, padding=(10, 20, 5, 5)))
-        album_questions_input_box.add(album_questions_name_input_title)
+        #this is the box that opens to the top that can add new albums
+        self.album_questions_box = toga.Box(style=Pack(background_color="#808080"))
+        album_questions_label = toga.Label("", style=Pack(flex=1.5, background_color="#808080"))
+        album_questions_answers_box = toga.Box(style=Pack(direction=COLUMN, background_color="#808080", flex=1))
+        album_questions_input_box = toga.Box(style=Pack(background_color="#808080", padding=(0,0,0,0)))
+        self.album_questions_name_input = toga.TextInput(placeholder="Album Name", style=Pack(flex=1, padding=(10, 20, 5, 5)))
         album_questions_input_box.add(self.album_questions_name_input)
         album_questions_answers_box.add(album_questions_input_box)
-        album_questions_buttons_box = toga.Box(style=Pack(background_color="#7b7b7b"))
+        album_questions_buttons_box = toga.Box(style=Pack(background_color="#808080"))
         album_questions_close_button = toga.Button("X", style=Pack(text_align=LEFT, font_size=10, padding=(0, 0, 5, 5)),on_press=self.close_album_questions)
         album_questions_submit_button = toga.Button("Submit",style=Pack(text_align=RIGHT, font_size=10, padding=(0, 20, 5, 5),flex=1),on_press=self.add_album_fun)
         album_questions_buttons_box.add(album_questions_close_button)
         album_questions_buttons_box.add(album_questions_submit_button)
         album_questions_answers_box.add(album_questions_buttons_box)
-        album_questions_box.add(album_questions_label)
-        album_questions_box.add(album_questions_answers_box)
+        self.album_questions_box.add(album_questions_label)
+        self.album_questions_box.add(album_questions_answers_box)
 
-        album_list_box = toga.Box(style=Pack(direction=COLUMN, flex=1))
+        album_list_box = toga.Box(style=Pack(direction=COLUMN, flex=1, background_color="#ffffff"))
         for i in range(len(self.albums)):
             album = self.albums[i].build()
             album_list_box.add(album)
-        album_list_scroll_box = toga.ScrollContainer(vertical=True, style=Pack(direction=COLUMN, flex=1),content=album_list_box)
+        self.album_list_scroll_box = toga.ScrollContainer(vertical=True, style=Pack(direction=COLUMN, flex=1),content=album_list_box)
 
-        self.sound_board_box = toga.Box(style=Pack(background_color="#686868"))
-        self.sound_board_fill_label = toga.Label("", style=Pack(flex=1, background_color="#686868"))
-        self.sound_board_button = toga.Button("Sound Board",style=Pack(text_align=RIGHT, font_size=10, padding=(10, 20, 10, 0)))
-        self.sound_board_box.add(self.sound_board_fill_label)
-        self.sound_board_box.add(self.sound_board_button)
+        #this is the soundboard box that can open
+        self.sound_board_box = toga.Box(style=Pack(background_color="#808080"))
+        self.sound_board_scroll_box = toga.ScrollContainer(vertical=True, style=Pack(direction=COLUMN, height=60, background_color="#6f6cf6"),content=self.sound_board_box)
 
-        self.box.add(album_add_box)
-        if self.add_album_questions == True:
-            self.box.add(album_questions_box)
-        self.box.add(album_list_scroll_box)
-        self.box.add(self.sound_board_box)
+        #this is the bottom box that holds the sound board button
+        self.sound_board_button_box = toga.Box(style=Pack(background_color="#800000"))
+        self.sound_board_button_fill_label = toga.Label("", style=Pack(flex=1, background_color="#800000"))
+        self.sound_board_button_button = toga.Button("Sound Board",style=Pack(text_align=RIGHT, font_size=10, padding=(10, 20, 10, 0)),on_press=self.toggel_sound_board)
+        self.sound_board_button_box.add(self.sound_board_button_fill_label)
+        self.sound_board_button_box.add(self.sound_board_button_button)
 
-        self.refresh_page(self.box)
+        self.refresh_box()
 
     def open_album_questions(self,what=None):
 
@@ -221,14 +266,42 @@ class MusicHockeyApp(App):
 
         name=self.album_questions_name_input.value
         self.albums.append(Album(name, self, len(self.albums)))
+        
+        project_dir = self.paths.data
+        new_album_dir = os.path.join(project_dir, f'albums\{name}')
+        if not os.path.exists(new_album_dir):
+                os.makedirs(new_album_dir)
+        else:
+            pass
+        
         self.main_page()
 
     def delete_album(self, number, what=None):
+
+        try:
+            project_dir = self.paths.data
+            album_dir = os.path.join(project_dir, 'albums')
+            target_dir = os.path.join(album_dir, self.albums[number].formal_name)
+            os.remove(target_dir)
+        except(PermissionError, FileNotFoundError):
+            print('perm error')
+            pass
+        
         self.albums.remove(self.albums[number])
+
         for i in range(len(self.albums)-number):
             i+=number
             self.albums[i].number-=1
         self.main_page()
 
+    def toggel_sound_board(self,what=None):
+        if self.sound_board_open == False:
+            self.sound_board_open = True
+        else:
+            self.sound_board_open = False
+        if self.current_page == "main_page":
+            self.main_page()
+        else:
+            self.albums[0].refresh_album_box()
 def main():
     return MusicHockeyApp()
