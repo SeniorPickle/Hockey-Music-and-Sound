89f5ac8f8f9d87b14ce2417f67c146391a235cd7
dylan <starkd@cbchs.org>
2024-11-23 17:39:47 -0600

    fix

--- musichockeyapp/src/musichockeyapp/music.py
+++ musichockeyapp/src/musichockeyapp/music.py
@@ -25,16 +25,16 @@ class Music():
             self.album.app.music_playing[1] = False
             self.album.app.albums[self.album.app.music_playing[0][0]].contents[self.album.app.music_playing[0][1]].music_playing = False
             pygame.mixer.music.pause()
-
-        pygame.mixer.music.load(self.path)
-        if self.music_playing == False:
-            self.music_playing = True
-            pygame.mixer.music.play()
-            self.album.app.music_playing = [[self.album.number, self.number], True, False]
         else:
-            self.music_playing = False
-            pygame.mixer.music.pause()
-            self.album.app.music_playing = [[None, None], False, False]
+            pygame.mixer.music.load(self.path)
+            if self.music_playing == False:
+                self.music_playing = True
+                pygame.mixer.music.play()
+                self.album.app.music_playing = [[self.album.number, self.number], True, False]
+            else:
+                self.music_playing = False
+                pygame.mixer.music.pause()
+                self.album.app.music_playing = [[None, None], False, False]
 
     def song_delete(self, what=None):
         pass
